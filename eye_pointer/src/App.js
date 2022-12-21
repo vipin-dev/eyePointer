2f808a2e52cd95f5c54bdd69e59c657c78513ab0
Vinay Dev S <69599514+vinay-devs@users.noreply.github.com>
2022-12-21 15:22:40 +0530

    creates workspace and aboutus-btn

--- eye_pointer/src/App.js
+++ eye_pointer/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@mui/material/Button";
+import { Box, margin } from "@mui/system";
+import bg from "./images/bg.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="webpage"
+      style={{ backgroundImage: `url(${bg})`, paddingBottom: "30px" }}
+    >
+      <Box sx={{ textAlign: "right", padding: "10px" }}>
+        <Button variant="contained">About Us</Button>
+      </Box>
+      <div className="workspace">
+        <h1>HEllo</h1>
+      </div>
     </div>
   );
 }
