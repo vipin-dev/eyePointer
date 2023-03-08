73924b04380a48e021144b6359ace907b4ad5885
Vinay Dev S <vinaydevs@gmail.com>
2023-03-08 14:18:31 +0530

    Addes position of the Tile

--- eye_pointer/src/App.js
+++ eye_pointer/src/App.js
@@ -1,50 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Tile from "./components/tile";
 import styled from "styled-components";
 import Button from "@mui/material-next/Button";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 function App() {
-    let text="Hello My Dear";
+  const [tiles, setTiles] = useState([1, 3]);
+
   return (
     <WorkPage>
-        <Buttons>
-            <Button size="small" variant="outlined" startIcon={<AddCircleIcon/>}> </Button>
-            <Button size="small" variant="outlined"> About Us </Button>
-        </Buttons>
-        <WorkSpace>
-            <Tile text={text} />
-        </WorkSpace>
+      <Buttons>
+        <Button
+          onClick={() => {
+            console.log("Hello");
+          }}
+          size="small"
+          variant="outlined"
+          startIcon={<AddCircleIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          size="small"
+          variant="outlined"
+          onClick={() => {
+            "Pressed About Us";
+          }}
+        >
+          {" "}
+          About Us{" "}
+        </Button>
+      </Buttons>
+      <WorkSpace>
+        {tiles.map((tile) => {
+          return <Tile />;
+        })}
+      </WorkSpace>
     </WorkPage>
   );
 }
 
-
 const WorkPage = styled.div`
-    overflow: "hidden";
-    position:relative;
-    height:700px;
-    width:1300px;
+  overflow: "hidden";
+  position: relative;
+  height: 700px;
+  width: 1300px;
 `;
 
-const WorkSpace=styled.div`
-    position:absolute;
-    left:66px;
-    top:66px;
-    right:66px;
-    height:600px;
-    width:inherit;
-    border: 6px solid black;
-    border-radius: 4px;
-    background-color: white;
-`
+const WorkSpace = styled.div`
+  position: relative;
+  left: 66px;
+  top: 66px;
+  height: 600px;
+  width: inherit;
+  border: 6px solid black;
+  border-radius: 4px;
+  background-color: white;
+`;
 
-const Buttons=styled.div`
-    position:absolute;
-    top:15px;
-    left:66px;
-    width:inherit;
-    display:flex;
-    justify-content:space-between;
-`
+const Buttons = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 66px;
+  width: inherit;
+  display: flex;
+  justify-content: space-between;
+`;
 export default App;
